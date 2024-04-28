6ca045ec0739b62fde0eb3cea122788f9e7d349d
dok404 <165073769+dok404@users.noreply.github.com>
2024-04-28 01:04:28 -0700

    Add files via upload

---
+++ main_page.py
@@ -0,0 +1,39 @@
+import streamlit as st
+
+st.set_page_config(page_title="Furniture Reformer", page_icon="images/logo.png", layout="wide")
+
+st.sidebar.image("images/furniture_reformer.png", width = 200)
+st.sidebar.page_link("main_page.py", label = "Home")
+st.sidebar.page_link("pages/page1.py", label = "Pick Up Furniture")
+st.sidebar.page_link("pages/page2.py", label = "Dispose/Donate/Sell Furniture")
+st.sidebar.page_link("pages/page3.py", label = "Determine Furniture's Condition")
+
+st.image("images/furniture_reformer.png", width = 525)
+
+st.title("Welcome!")
+message = "What would you like to do today?"
+st.header(message)
+
+st.divider()  # Add a horizontal line for separation
+
+feature_1 = "Pick Up Furniture"
+st.title(feature_1)
+description_1 = "Advises you where you can pick up furnitures."
+st.header(description_1)
+st.page_link("pages/page1.py", label = ":orange[Click here to find out more]")
+
+st.divider()  # Add a horizontal line for separation
+
+feature_2 = "Dispose/Donate/Sell Furniture"
+st.title(feature_2)
+description_2 = "Advises you how to donate, sell, or dispose furnitures."
+st.header(description_2)
+st.page_link("pages/page2.py", label = ":orange[Click here to find out more]")
+
+st.divider()  # Add a horizontal line for separation
+
+feature_3 = "Determine Furniture's Condition"
+st.title(feature_3)
+description_3 = "Determines the condition of furniture and advises you how to maintain, repair, or dismantle it depending on the condition."
+st.header(description_3)
+st.page_link("pages/page3.py", label = ":orange[Click here to find out more]")
